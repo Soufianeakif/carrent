bb579d5524827d4228adae2179b2e2e7c88345b7
Soufiane Akif <75849464+Soufianeakif@users.noreply.github.com>
2024-06-12 05:23:27 +0100

    add Testimonials and brands on landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -215,9 +215,39 @@
             </div>
         </div>
     </div>
+    <section class="bg-white dark:bg-gray-900">
+  <div class="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
+      <figure class="max-w-screen-md mx-auto">
+          <svg class="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/>
+          </svg> 
+          <blockquote>
+            <p class="text-2xl font-medium text-gray-900 dark:text-white">"RentCar is amazing. They offer a wide variety of vehicles and the booking process is quick and easy. Perfect for your next vacation or business trip."</p>          </blockquote>
+          <figcaption class="flex items-center justify-center mt-6 space-x-3">
+              <img class="w-6 h-6 rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAgX6TNEj3QBH5AloHimts73J6rVrUsLboxQ&s" alt="profile picture">
+              <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                  <div class="pr-3 font-medium text-gray-900 dark:text-white">Youssef</div>
+                  <div class="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">Morocco national team | Sevilla Football player</div>
+              </div>
+          </figcaption>
+      </figure>
+  </div>
+</section>
 </div>
 
 
+
+<div class="bg-white py-24 sm:py-32">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+    <img class="col-span-2 col-start-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1" src="https://logodownload.org/wp-content/uploads/2014/09/renault-logo-2-1.png" alt="Statamic">
+    <img class="col-span-2 col-start-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1" src="https://iconape.com/wp-content/png_logo_vector/bmw-logo.png" alt="Statamic">
+    <img class="col-span-2 col-start-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1" src="https://www.freepnglogos.com/uploads/mercedes-logo-png/mercedes-logo-home-page-palm-beach-classics-8.png" alt="Statamic">
+    <img class="col-span-2 col-start-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1" src="https://logolook.net/wp-content/uploads/2023/12/Dacia-Logo.png" alt="Statamic">
+    <img class="col-span-2 col-start-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1" src="https://www.logo-voiture.com/wp-content/uploads/2023/02/toyota-logo-europe-1.png" alt="Statamic">
+    </div>
+  </div>
+</div>
 
 
 
